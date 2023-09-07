f7a6081bb96ae094fbd6b0853a22db0b5ee10588
freni123 <frenivaghasiya@gmail.com>
2023-09-07 17:05:22 +0530

    07/09/23 config,dbconnection file

---
+++ Homework/E-commarceApp/index.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const express = require('express');
+
+// http module
+// http.createServer((req, res) => {
+//     res.writeHead(200);
+//     res.end('hello world\n');
+//   }).listen(8000);
+
+// express using server
+const app = express();
+// app.get('/',(req,res)=> {
+//     res.send('Express server is Running');
+//     console.warn('Express server is Running');
+//     res.end()
+// }).listen(4500);
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(bodyParser.json());
+
+//Routes Namespace upload files
+app.use("/v1", routes);
+
+app.use((req,res,next) => {
+    next(new Error("Route not Found!"));
+});
+//Database connection
+connectDB();
+
+// create server using http
+const server = http.createServer(app);
+
+server.listen(config.port, () => {
+    console.log("server listening");
+})
+
